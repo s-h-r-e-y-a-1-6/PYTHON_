d5578453de45f649d2ecefbc3ab11f01c113f71e
s-h-r-e-y-a-1-6 <sshreya2_be23@thapar.edu>
2024-09-21 01:47:17 +0530

    15.py

--- worksheet1/15.py
+++ worksheet1/15.py
@@ -1,2 +1,5 @@
-numbers = list(map(int, input("Enter the list of numbers separated by space: ").split()))
-print("Sum of all elements:", sum(numbers))
+num = int(input("Enter a number: "))
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+print("Factorial of", num, "is", factorial)
