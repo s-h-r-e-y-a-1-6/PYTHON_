c133d96f9e9973da97ee136fe17b65a8c0675907
s-h-r-e-y-a-1-6 <sshreya2_be23@thapar.edu>
2024-09-21 01:49:00 +0530

    12.py

--- worksheet1/12.py
+++ worksheet1/12.py
@@ -1,5 +1,16 @@
-P = float(input("Enter the principal amount: "))
-R = float(input("Enter the rate of interest: "))
-T = float(input("Enter the time (in years): "))
-simple_interest = (P * R * T) / 100
-print("Simple Interest:", simple_interest)
+def triangle(angle1, angle2, angle3):
+    # Sum of angles must be 180 degrees, and each angle should be greater than 0
+    if angle1 > 0 and angle2 > 0 and angle3 > 0 and (angle1 + angle2 + angle3 == 180):
+        return True
+    else:
+        return False
+
+angle1 = float(input("Enter the first angle: "))
+angle2 = float(input("Enter the second angle: "))
+angle3 = float(input("Enter the third angle: "))
+
+# Checking if the angles form a valid triangle
+if triangle(angle1, angle2, angle3):
+    print("The angles form a valid triangle.")
+else:
+    print("The angles do not form a valid triangle.")
